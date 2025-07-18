33f9e363122a73a6432ed8b154b0e291dc65eede
MasterPigeon <lafo.edo@gmail.com>
2025-07-18 17:39:41 -0400

    Doooooooooooor

--- sketch.js
+++ sketch.js
@@ -13,9 +13,6 @@ function preload() {
 }
 
 
-
-
-
 function setup() {
 	new Canvas(innerWidth / 4, innerHeight / 4, 'pixelated x4');
 
@@ -45,7 +42,7 @@ function setup() {
 		player.rotationLock = true;
 		player.spriteSheet = playerSheet;
 		allSprites.pixelPerfect = true;
-		player.dashCooldown = 100;
+		player.dashCooldown = 75;
 		player.coins = 0;
 		player.vies = 3;
 		player.addAnis({
@@ -58,14 +55,16 @@ function setup() {
 		player.changeAni('idle');
 	}
 
-	{player.feet = new Sprite();
+	{
+		player.feet = new Sprite();
 		player.feet.collider = 'none';
 		player.feet.debug = true;
 		player.feet.w = 8;
 		player.feet.h = 5;
 	}
 
-	{player.head = new Sprite();
+	{
+		player.head = new Sprite();
 		player.head.collider = 'none';
 		player.head.debug = true;
 		player.head.w = 8;
@@ -73,7 +72,8 @@ function setup() {
 		player.head.visible = true;
 	}
 
-	{grass = new Group();
+	{
+		grass = new Group();
 		grass.collider = 'static';
 		grass.w = 15;
 		grass.h = 16;
@@ -83,7 +83,8 @@ function setup() {
 		grass.friction = 4;
 	}
 
-	{fakegrass = new Group();
+	{
+		fakegrass = new Group();
 		fakegrass.collider = 'none';
 		fakegrass.w = 15;
 		fakegrass.h = 16;
@@ -93,7 +94,8 @@ function setup() {
 		fakegrass.friction = 2;
 	}
 
-	{dirt = new Group();
+	{
+		dirt = new Group();
 		dirt.collider = 'static';
 		dirt.w = 15;
 		dirt.h = 16;
@@ -103,7 +105,8 @@ function setup() {
 		dirt.friction = 4;
 	}
 
-	{fakedirt = new Group();
+	{
+		fakedirt = new Group();
 		fakedirt.collider = 'none';
 		fakedirt.w = 15;
 		fakedirt.h = 16;
@@ -113,7 +116,8 @@ function setup() {
 		fakedirt.friction = 2;
 	}
 
-	{wall = new Group();
+	{
+		wall = new Group();
 		wall.collider = 'static';
 		wall.w = 15;
 		wall.h = 16;
@@ -123,7 +127,8 @@ function setup() {
 		wall.friction = 2;
 	}
 
-	{champignon = new Group();
+	{
+		champignon = new Group();
 		champignon.collider = 'static';
 		champignon.w = 15;
 		champignon.h = 16;
@@ -133,7 +138,8 @@ function setup() {
 		champignon.friction = 4;
 	}
 
-	{champignon2 = new Group();
+	{
+		champignon2 = new Group();
 		champignon2.collider = 'static';
 		champignon2.w = 15;
 		champignon2.h = 16;
@@ -143,7 +149,8 @@ function setup() {
 		champignon2.friction = 4;
 	}
 
-	{ice = new Group();
+	{
+		ice = new Group();
 		ice.collider = 'static';
 		ice.w = 15;
 		ice.h = 16;
@@ -153,7 +160,8 @@ function setup() {
 		ice.friction = 0.5;
 	}
 
-	{buisson = new Group();
+	{
+		buisson = new Group();
 		buisson.collider = 'none';
 		buisson.w = 15;
 		buisson.h = 16;
@@ -163,7 +171,8 @@ function setup() {
 		buisson.friction = 2;
 	}
 
-	{broken_blocks = new Group();
+	{
+		broken_blocks = new Group();
 		broken_blocks.collider = 'static';
 		broken_blocks.w = 15;
 		broken_blocks.h = 16;
@@ -173,7 +182,8 @@ function setup() {
 		broken_blocks.friction = 2;
 	}
 
-	{dead_knight = new Group();
+	{
+		dead_knight = new Group();
 		dead_knight.collider = 'none';
 		dead_knight.w = 15;
 		dead_knight.h = 16;
@@ -185,7 +195,8 @@ function setup() {
 		dead_knight.anis.offset.x = -4;
 	}
 
-	{coins = new Group();
+	{
+		coins = new Group();
 		coins.collider = 'none';
 		coins.w = 15;
 		coins.h = 16;
@@ -195,7 +206,8 @@ function setup() {
 		coins.friction = 2;
 	}
 
-	{signs = new Group();
+	{
+		signs = new Group();
 		signs.collider = 'none';
 		signs.w = 15;
 		signs.h = 16;
@@ -205,7 +217,8 @@ function setup() {
 		signs.friction = 2;
 	}
 
-	{tree = new Group();
+	{
+		tree = new Group();
 		tree.collider = 'none';
 		tree.w = 15;
 		tree.h = 16;
@@ -215,7 +228,8 @@ function setup() {
 		tree.friction = 2;
 	}
 
-	{bridge = new Group();
+	{
+		bridge = new Group();
 		bridge.collider = 'static';
 		bridge.w = 15;
 		bridge.h = 16;
@@ -225,7 +239,8 @@ function setup() {
 		bridge.friction = 2;
 	}
 
-	{doors = new Group();
+	{
+		doors = new Group();
 		doors.collider = 'static';
 		doors.w = 15;
 		doors.h = 16;
@@ -236,7 +251,8 @@ function setup() {
 		doors.openTime = 0;
 	}
 
-	{bottle = new Group();
+	{
+		bottle = new Group();
 		bottle.collider = 'none';
 		bottle.w = 15;
 		bottle.h = 16;
@@ -245,7 +261,7 @@ function setup() {
 		bottle.addAni({ w: 16, h: 16, col: 1, row: 7 });
 		bottle.friction = 4;
 	}
-	
+
 	world.gravity.y = 9.81;
 
 	tiles = new Tiles(level1, 0, 0, 16, 16);
@@ -263,14 +279,15 @@ async function draw() {
 
 		player.head.x = player.x;
 		player.head.y = player.y - 8;
-	
-	slime.moveTo(player.x, undefined, 1);
 
-	if (slime.x > player.x) {
-		slime.mirror = true;
-	} else {
-		slime.mirror.x = false;
-	}}
+		slime.moveTo(player.x, undefined, 1);
+
+		if (slime.x > player.x) {
+			slime.mirror = true;
+		} else {
+			slime.mirror.x = false;
+		}
+	}
 
 	for (let sign of signs) {
 		if (player.overlapping(sign) && kb.pressing('e')) {
@@ -288,97 +305,101 @@ async function draw() {
 			broken_block.collider = 'dinamic';
 		}
 	}
-	{for (let door of doors) {
-		if (kb.pressing('d')) {
-			door.openTime = 200;
+	{ 
+		if (kb.pressing('e')) {
+			doors.openTime = 200;
+			print('open doors')
 		}
-	}
+	
 	if (doors.openTime <= 0) {
 		doors.collider = 'static';
 	} else {
 		doors.collider = 'none';
 	}
 	doors.openTime = doors.openTime - 1;
+
 	}
 
 	//gameOver, slime hit, idle animation
 	{
-	if (player.vies <= 0 || player.y > 1000) {
-		await gameOver();
-	} else {
-		if (player.feet.overlaps(slime)) {
-			await player.changeAni('hit');
-			player.vies = player.vies - 1;
-			player.changeAni('idle');
-			slime.moveAway(player, 0.4);
-			player.moveAway(slime, 0.2);
+		if (player.vies <= 0 || player.y > 1000) {
+			await gameOver();
 		} else {
-			if (abs(player.vel.x) < 0.15) {
-			player.changeAni('idle');
+			if (player.collide(slime)) {
+				await player.changeAni('hit');
+				player.vies = player.vies - 1;
+				player.changeAni('idle');
+				slime.moveAway(player, 0.4);
+				player.moveAway(slime, 0.2);
+			} else {
+				if (abs(player.vel.x) < 0.15) {
+					player.changeAni('idle');
+				}
+			}
 		}
-		}		
-	}}
+	}
 
 	//propultion des champignon
 	{
-	if (player.feet.collide(champignon)) {
-		player.vel.y = -4;
-	}
+		if (player.feet.collide(champignon)) {
+			player.vel.y = -4;
+		}
 
-	if (player.feet.collide(champignon2)) {
-		player.vel.y = -7;
-	}
+		if (player.feet.collide(champignon2)) {
+			player.vel.y = -7;
+		}
 	}
 	//player action
 	{
-	if (player.feet.overlapping(tiles)) {
-		if (kb.pressing('up')) {
-			player.vel.y = -3;
+		if (player.feet.overlapping(tiles)) {
+			if (kb.pressing('up')) {
+				player.vel.y = -3;
+			}
+		}
+
+		if (kb.pressing('left')) {
+			player.vel.x = -2;
+			player.mirror.x = true;
+		}
+
+		if (kb.pressing('right')) {
+			player.vel.x = 2;
+			player.mirror.x = false;
+		}
+
+		if (kb.pressing('down')) {
+			player.vel.y = 0.5;
+		}
+
+		//dash
+		{
+			if (kb.pressing('space') && player.dashCooldown < 0) {
+				player.changeAni('dash');
+				player.changeAni('endDash');
+				if (player.mirror.x) {
+					player.vel.x = -5;
+				} else {
+					player.vel.x = 6;
+				}
+				player.dashCooldown = 100;
+			}
+			player.dashCooldown = player.dashCooldown - 1;
 		}
 	}
 
-	if (kb.pressing('left')) {
-		player.vel.x = -2;
-		player.mirror.x = true;
-	}
-
-	if (kb.pressing('right')) {
-		player.vel.x = 2;
-		player.mirror.x = false;
-	}
-
-	if (kb.pressing('down')) {
-		player.vel.y = 0.5;
-	}
-
-	//dash
-	{if (kb.pressing('space') && player.dashCooldown < 0) {
-		player.changeAni('dash');
-		player.changeAni('endDash');
-		if (player.mirror.x) {
-			player.vel.x = -5;
-		} else {
-			player.vel.x = 6;
-		}
-		player.dashCooldown = 100;
-	}
-	player.dashCooldown = player.dashCooldown - 1;
-	}
-	}
-
-text('Coins: ' + player.coins, 5, 15);
-text('Vie: ' + player.vies, 5, 30);
-text('FPS: ' + round(frameRate()), 5, 45);
+	text('Coins: ' + player.coins, 5, 15);
+	text('Vie: ' + player.vies, 5, 30);
+	text('FPS: ' + round(frameRate()), 5, 45);
 
 	{// changement de niveau
-	if (player.overlaps(bottle)) {
-		currentLevel = currentLevel + 1;
-		tiles.remove();
-		tiles = new Tiles(levels[currentLevel], 0, 0, 16, 16);
-		player.x = width / 2;
-		player.y = 0;
+		if (player.overlaps(bottle)) {
+			currentLevel = currentLevel + 1;
+			tiles.remove();
+			tiles = new Tiles(levels[currentLevel], 0, 0, 16, 16);
+			player.x = width / 2;
+			player.y = 0;
+		}
 	}
-}
 }
 
 async function gameOver() {
